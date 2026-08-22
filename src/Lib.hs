8fb3ed351284b586b9c56c2a4185bb83c1b7fa73
antonvedaet <antonvedernikov2003@gmail.com>
2026-08-22 04:41:53 +0300

    wip alpha conversion

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,31 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Char (ord, chr)
+
+data Term =
+    Variable Char
+  | Abstraction Char Term
+  | Application Term Term 
+  deriving(Show)
+
+next :: Char -> Char
+next 'a' = 'z'
+next x = chr (ord x + 1)
+
+collectUsed :: Term -> [Char]
+collectUsed (Variable c) = [c] 
+collectUsed (Abstraction c t) = [c] ++ collectUsed t
+collectUsed (Application x y) = collectUsed x ++ collectUsed y
+
+
+alphaConvert :: Term -> Char -> Term
+alphaConvert (Abstraction old t) new = Abstraction new (rename t new old ([old] ++ collectUsed(t)))
+alphaConvert term _ = term
+
+rename :: Term -> Char -> Char -> [Char] -> Term
+rename term new old used = term -- TODO implemented
+
 someFunc :: IO ()
-someFunc = putStrLn "Я скоро стану интерпретатором лямбда исчесления"
+someFunc = print $ alphaConvert (Abstraction 'x' (Abstraction 'y' (Variable 'x'))) 'y'
 
